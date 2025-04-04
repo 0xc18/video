3262da51f638fd644f6456562624bfc8e8c96e42
SAKIB-SALIM <144510317+SAKIB-SALIM@users.noreply.github.com>
2025-04-04 20:54:14 +0600

    whhw

--- upload.py
+++ upload.py
@@ -2,6 +2,10 @@ import requests
 import urllib.parse
 import os
 
+proxies = {
+    'http': 'socks5h://127.0.0.1:9050',
+    'https': 'socks5h://127.0.0.1:9050'
+}
 
 def upload():
     file_path = 'output/'+os.listdir('output')[0]
@@ -10,7 +14,7 @@ def upload():
         encoded_name = urllib.parse.quote(file_name)
         url = f"https://filebin.net/{os.urandom(5).hex()}/{encoded_name}"
         with open(file_path, 'rb') as f:
-            response = requests.post(url, data=f)
+            response = requests.post(url, data=f,proxies=proxies)
         print("\nStatus Code:", response.status_code)
         print("Response Text:", response.text)
         print(url)
